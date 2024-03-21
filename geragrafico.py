bbf617c97f81ca7f9ac77d9aa8a45eb6fca3ecd3
Joao Vicente <joao-vnfeltrin@educar.rs.gov.br>
2024-03-21 09:33:34 -0300

    Calculando médias das séries e plotando gráfico de barras

--- geragrafico.py
+++ geragrafico.py
@@ -9,6 +9,8 @@ def main():
     valores = leitor.getValores()
     print(valores)
 
+    plt.subplot(1, 2, 1)
+
     plt.title('Gráfico de linhas')
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
@@ -18,6 +20,16 @@ def main():
        plt.plot(serie, label='Série ' + str(i))
        i += 1
        plt.legend(loc='upper left')
+    
+    plt.subplot(1, 2, 2)
+   
+    medias = leitor.getMedias()
+    xvalues = np.arange(1, len(medias)+1)
+    plt.bar(xvalues, medias)  
+    plt.xticks(xvalues, ['Série ' + str(x) for x in xvalues])
+    plt.title('Médias das séries')
+   
     plt.show()
+    
 
 main()
